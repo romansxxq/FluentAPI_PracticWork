8df6dc148ecf9fd3cdcefe3333d703dfe5e5a1dd
mka <mka@A26PC15>
2024-08-24 11:38:23 +0300

    Add project files.

---
+++ FluentAPI/Cities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI
+{
+    public class Cities
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [Required, MaxLength(100)]
+        public int CountryId { get; set; }
+        [Required, MaxLength(100)]
+        
+        public ICollection<Shop> Shops { get; set; }
+    }
+}
---
+++ FluentAPI/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        public string CityId { get; set; }
+    }
+}
